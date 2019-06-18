00d2ef4c157c5d27b8fe6b35a943931a60864117
dollop <21363432+lentil01@users.noreply.github.com>
2019-06-18 12:31:28 -0400

    updated jquery version, added conditions for mousewheel js to be called only at certain screen sizes

--- jquery-custom.js
+++ jquery-custom.js
@@ -1,6 +1,10 @@
-$(document).ready(function() {
-    $('html, body, *').mousewheel(function(e, delta) {
-    this.scrollLeft -= (delta);
-    e.preventDefault();
-    });
-    });
+$(document).ready(function () {
+
+    if ($(window).width() >= 993) {
+        $('html, body, *').mousewheel(function (e, delta) {
+            this.scrollLeft -= (delta);
+            e.preventDefault();
+        });
+    }
+
+});
