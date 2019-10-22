afda48b63897d93797db2f2ee3c4e0cf6a3988d5
dollop <dollop@cs-MacBook-Pro.local>
2019-10-22 17:41:11 -0400

    reverted 1 change regarding responsiveness

--- js/jquery-custom.js
+++ js/jquery-custom.js
@@ -1,19 +1,12 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     if ($(window).width() >= 993) {
+    if ($(window).width() >= 993) {
         
-//         $('html, body, *').mousewheel(function (evt, delta) {
-//             this.scrollLeft -= (delta);
-//             evt.preventDefault();
-//         });
+        $('html, body, *').mousewheel(function (evt, delta) {
+            this.scrollLeft -= (delta);
+            evt.preventDefault();
+        });
 
         
-//     }
-// });
-
-$(document).ready(function() {
-    $('html, body, *').mousewheel(function(e, delta) {
-    this.scrollLeft -= (delta);
-    e.preventDefault();
-    });
-    });
+    }
+});
